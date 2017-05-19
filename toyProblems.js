b9af303b7ebd20245c892e8c82cd3d49c10050bd
Timothy Daniel Mitchell <m.timothyd@gmail.com>
2017-05-19 11:49:29 -0600

    green

--- toyProblems.js
+++ toyProblems.js
@@ -1,27 +1,33 @@
 //JS Basics
 //Run npm test in the command line to test your solutions
 module.exports = {
-  // reverseIt:
+  reverseIt: (str)=>str.split("").reverse().join(""),
 
   //uncomment and finish the reverseIt function. It will take in one parameter which is a String and
   //reverse it
 
-  // removeDups:
+  removeDups:(arr)=>arr.filter((e,i)=> i === arr.indexOf(e)),
 
   //uncomment and finish the removeDups function. It will take in one parameter which is an Array
   //remove all duplicates
 
-  // titleIt:
+  titleIt:(str)=>str.split(" ").map(e=>e[0].toUpperCase() + e.slice(1)).join(" "),
 
   //uncomment and finish the titleIt function. It will take in one parameter which is a String and
   //capitalize the first letter of each word
 
-  // vowelCounter:
+  vowelCounter:(str)=>str.toLowerCase().match(/[aeiou]/g).length,
 
   //uncomment and finish the vowelCounter function. It will take in one parameter which is a String and
   //return the number of vowels in the string
 
-  // isPrime:
+  isPrime:(num)=> {
+    if(num < 1) return false;
+    for(let i = num - 1; i > 1; i--) {
+      if(!(num%i)) return false
+    }
+    return true
+  },
 
   //uncomment and finish the isPrime function. It will take in one parameter which is a Number and
   //return true if it is prime and false if it is not
@@ -30,7 +36,7 @@ module.exports = {
   //var foo = 10 + '20';
   //uncomment the foo property and give your answer as its value
 
-  //foo:
+  foo:'1020',
 
   //what is the outcome of the two console.logs below?
   //   var foo = "Hello";
@@ -41,6 +47,6 @@ module.exports = {
   // console.log(foo + bar);
   // uncomment the log1 and log2 properties and give your answers as their values
 
-  // log1:
-  // log2:
+  log1:'Hello World',
+  log2:undefined
 }
